3f372850127a5b3550e651b3331ee698507ed358
Jonathanaloya <jonathanaloya27@gmail.com>
2024-02-02 23:03:58 -0800

    Activity

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,30 @@
 using System;
-
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
-    }
+        Console.WriteLine("Welcome to the Activity Menu!");
+        Console.WriteLine("Please select an activity:");
+        Console.WriteLine("1. Start reflecting activity");
+        Console.WriteLine("2. Start breathing activity");
+        Console.WriteLine("3. Start listing activity");
+        Console.WriteLine("4. Quit");
+        int choice = int.Parse(Console.ReadLine());
+
+        switch (choice)
+        {
+            case 1:
+                ReflectionActivity reflection = new ReflectionActivity();
+                reflection.Run();
+                break;
+            case 2:
+                BreathingActivity breathing = new BreathingActivity();
+                breathing.Run();
+                break;
+            case 3:
+                ListingActivity listing = new ListingActivity();
+                listing.Run();
+                break;
+        }   }
+        
 }
