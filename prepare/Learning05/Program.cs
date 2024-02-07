50bf456c476d29ca9bede076799a8a0d0d85c6c1
Jonathanaloya <jonathanaloya27@gmail.com>
2024-02-07 02:49:28 -0800

    Shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,25 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+            
+        Square square = new Square("Red", 3);
+        Rectangle rectangle = new Rectangle("Blue", 4, 5);
+        Circle circle = new Circle(6, "Green");
+            
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+            
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+        }
     }
 }
+    
+
